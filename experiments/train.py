134c1e16fad8f21b6fc80be2af0028fed6f366a4
PenelopeJones <pj321@cam.ac.uk>
2021-08-30 14:10:21 +0100

    Adding experiments

--- experiments/train.py
+++ experiments/train.py
@@ -62,17 +62,25 @@ class VanillaNN(nn.Module):
 
         return self.layers[-1](x)
 
+    def predict(self, data, mu_x=None, std_x=None, n_systems=5, n_samples=500, ptd='../data/processed/'):
+        X_batch, y_batch, y_batch_err = sample_batch(data, mu_x, std_x, n_systems, n_samples, ptd)
+        local_pred_batch = self.forward(X_batch)
+        local_pred_batch = local_pred_batch.reshape(n_systems, n_samples, 1)
+        # y is the mean over all samples
+        pred_batch = torch.mean(local_pred_batch, dim=1)
+        return pred_batch, y_batch, y_batch_err
+
 def system_subsample(conc, lb, n_samples, ptd):
     ptf = ptd + 'X_{}_{}'.format(conc, lb).replace('.', '-') + '.npy'
     x = np.load(ptf)
     idx = np.random.choice(x.shape[0], size=n_samples, replace=False)
     return x[idx]
 
-def sample_batch(concs, lbs, y, y_err, mu_x=None, std_x=None, n_systems=5, n_samples=500, ptd='../data/processed/'):
+def sample_batch(data, mu_x=None, std_x=None, n_systems=5, n_samples=500, ptd='../data/processed/'):
+    (y, y_err, concs, lbs) = data
     nt = concs.shape[0]
     assert concs.shape[0] == lbs.shape[0] == y.shape[0] == y_err.shape[0]
-    ids = np.random.choice(99, size=n_systems, replace=False)
-    #ids = np.random.choice(nt, size=n_systems, replace=False)
+    ids = np.random.choice(nt, size=n_systems, replace=False)
     y_batch = torch.tensor(y[ids]).float()
     y_batch_err = torch.tensor(y_err[ids]).float()
     X_batch = []
@@ -135,7 +143,16 @@ def main(args):
     concs = concs.repeat(9).reshape(-1)
     lbs = np.load(ptd + 'bjerrum_lengths.npy')
     lbs = np.tile(lbs, 12).reshape(-1)
+
+    y = y[0:99]
+    y_err = y_err[0:99]
+    concs = concs[0:99]
+    lbs = lbs[0:99]
+
     data_train, data_valid, data_test = train_test_split(y, y_err, concs, lbs, seed=seed)
+    n_train = data_train[0].shape[0]
+    n_valid = data_valid[0].shape[0]
+    n_test = data_test[0].shape[0]
     (y_train, y_err_train, concs_train, lbs_train) = data_train
     pdb.set_trace()
     # Get scalers, and scale the y_data
@@ -161,11 +178,11 @@ def main(args):
     for epoch in range(epochs):
         optimiser.zero_grad()
 
-        X_batch, y_batch, y_batch_err = sample_batch(concs, lbs, y, y_err, mu_x, std_x, n_systems, n_samples, ptx)
+        X_batch, y_batch, y_batch_err = sample_batch(data_train, mu_x, std_x, n_systems, n_samples, ptx)
 
         local_pred_batch = model(X_batch)
 
-        local_pred_batch = local_pred_batch.reshape(n_systems, n_samples, -1)
+        local_pred_batch = local_pred_batch.reshape(n_systems, n_samples, 1)
 
         # y is the mean over all samples
         pred_batch = torch.mean(local_pred_batch, dim=1)
@@ -176,21 +193,21 @@ def main(args):
             print('Epoch {}\tLoss: {}'.format(epoch, running_loss / print_freq))
             running_loss = 0
             pdb.set_trace()
-            """
-            pred_train = model(X_train).float().detach().numpy()
-            pred_test = model(X_test).float().detach().numpy()
-            pred_train = sc_y.inverse_transform(pred_train)
-            pred_test = sc_y.inverse_transform(pred_test)
-            true_train = sc_y.inverse_transform(y_train.float().detach().numpy())
-            true_test = y_test
 
-            print('Train RMSE: {:.2f}\t Test RMSE: {:.2f}\t Train R2: {:.2f}\t Test R2: {:.2f}\n'.format(np.sqrt(mean_squared_error(true_train, pred_train)),
-                                                                                         np.sqrt(mean_squared_error(true_test, pred_test)),
-                                                                                         r2_score(true_train, pred_train),
-                                                                                         r2_score(true_test, pred_test)))
-            """
+            pred_tr, y_tr, y_err_tr = model.predict(data_train, n_systems=n_train)
+            pred_val, y_val, y_err_val = model.predict(data_valid, n_systems=n_valid)
+            pred_tr = sc_y.inverse_transform(pred_tr)
+            y_tr = sc_y.inverse_transform(y_tr)
+            pred_val = sc_y.inverse_transform(pred_val)
+            y_val = sc_y.inverse_transform(y_val)
+            print('Train RMSE: {:.2f}\t Valid RMSE: {:.2f}\t Train R2: {:.2f}\t Valid R2: {:.2f}\n'.format(np.sqrt(mean_squared_error(y_tr, pred_tr)),
+                                                                                         np.sqrt(mean_squared_error(y_val, pred_val)),
+                                                                                         r2_score(y_tr, pred_tr),
+                                                                                         r2_score(y_val, pred_val)))
+
         loss.backward()
         optimiser.step()
+
 
 
 if __name__ == "__main__":
