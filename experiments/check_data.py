8b6d7ce129c8f4dece7f031f03777638e609d7b4
PenelopeJones <pj321@cam.ac.uk>
2021-09-27 15:29:59 +0100

    Adding data inspector

--- experiments/check_data.py
+++ experiments/check_data.py
@@ -10,7 +10,7 @@ import pdb
 
 
 def main(args):
-    ptd = '../../data/processed/'
+    ptd = '../data/'
 
     # Load ion positions
     y = np.load(ptd + 'molar_conductivities.npy')
@@ -18,20 +18,20 @@ def main(args):
     y_err = np.load(ptd + 'molar_conductivities_error.npy')
     y_err = y_err.reshape(-1, 1)
     concs = np.load(ptd + 'concentrations.npy')
-    concs = concs.repeat(9).reshape(-1)
     lbs = np.load(ptd + 'bjerrum_lengths.npy')
-    lbs = np.tile(lbs, 12).reshape(-1)
 
+    errors = []
     for conc in concs:
         for lb in lbs:
-            ptf = ptd + 'X_{}_{}_soap'.format(conc, lb).replace('.', '-') + '.npy'
-            print('\nConc: {}\t lB: {}'.format(conc, lb))
+            ptf = ptd + 'processed/X_{}_{}_soap'.format(conc, lb).replace('.', '-') + '.npy'
+
             try:
                 X = np.load(ptf, allow_pickle=True)
+                print('\nConc: {}\t lB: {}\tN:{}, Dim:{}'.format(conc, lb, X.shape[0], X.shape[1]))
                 print(X.shape)
             except:
-                pdb.set_trace()
-                print('Error in loading data...')
+                errors.append('Conc: {}\t lB: {}\t\n'.format(conc, lb))
+    print(errors)
 
 
 
