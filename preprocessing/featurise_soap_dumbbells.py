dc752b6f26a492347b9f73b8caba950b9b583d90
PenelopeJones <pj321@cam.ac.uk>
2022-01-19 11:15:21 +0000

    Adding labels for dumbbell/no dumbbell

--- preprocessing/featurise_soap_dumbbells.py
+++ preprocessing/featurise_soap_dumbbells.py
@@ -7,8 +7,7 @@ import numpy as np
 from ase import Atoms
 from dscribe.descriptors import SOAP, ACSF
 
-from utils.feature_util import static_feature_vector
-from utils.mda_util import mda_to_numpy
+from utils.mda_util_dumbbells import mda_to_numpy
 
 import pdb
 
@@ -26,19 +25,26 @@ def main(args):
     sparse = False
 
     ptf = pts + 'X_dumbbells_{}_{}_soap'.format(conc, lb).replace('.', '-') + '.npy'
+    ptfl = pts + 'label_dumbbells_{}_{}_soap'.format(conc, lb).replace('.', '-') + '.npy'
 
-    anion_positions, cation_positions, solvent_positions, box_length = mda_to_numpy(conc, lb, ptd, dumbbells=True)
+    # extract positions of free anions, paired anions and all free/paired cations
+    anion_free_positions, anion_paired_positions, anion_positions, cation_positions, _, box_length = mda_to_numpy(conc, lb, ptd)
 
     assert anion_positions.shape == cation_positions.shape
+
     (n_snapshots, n_anions, _) = anion_positions.shape
     n_cations = cation_positions.shape[1]
     n_solvents = solvent_positions.shape[1]
+    n_anions_free = anion_free_positions.shape[1]
+    n_anions_paired = anion_paired_positions.shape[1]
     print(n_cations)
+    pdb.set_trace()
 
     cat_sym = 'Na'
     an_sym = 'Cl'
     species = [an_sym, cat_sym]
     symbols = [an_sym]*n_anions + [cat_sym]*n_cations
+
     soap_generator = SOAP(species=species, periodic=True,
                           rcut=rcut, nmax=nmax, lmax=lmax,
                           sparse=sparse)
@@ -56,7 +62,8 @@ def main(args):
     print('Concentration {}\t lB {}'.format(conc, lb))
 
     i = 0
-
+    label = np.array([1]*n_anions_free + [0]*n_anions_paired)
+    labels = []
 
     for snapshot_id in range(0, n_snapshots, max(1, skip_snaps)):
         t0 = time.time()
@@ -64,15 +71,22 @@ def main(args):
         # Select ion positions at a given snapshot
         anions = anion_positions[snapshot_id, :, :]
         cations = cation_positions[snapshot_id, :, :]
-        #solvents = solvent_positions[snapshot_id, :, :]
-        positions = np.vstack([anions, cations])
+        anions_free = anion_free_positions[snapshot_id, :, :]
+        anions_paired = anion_paired_positions[snapshot_id, :, :]
+
+        # solvents = solvent_positions[snapshot_id, :, :].
+        # first n_anions_free positions are free anions, then n_anions_paired are paired anions etc.
+        positions = np.vstack([anions_free, anions_paired, cations])
+
         system = Atoms(symbols=symbols, positions=positions,
                        cell=[box_length, box_length, box_length],
                        pbc=True)
         soap = soap_generator.create(system, positions=list(range(0, n_anions)))
         x.append(soap)
+        labels.append(label)
 
         np.save(ptf, np.vstack(x))
+        np.save(ptfl, np.hstack(labels))
         if i % args.print_freq == 0:
             print('Snapshot {}\t Time: {:.1f}'.format(i, (time.time() - t0)))
         i+=1
---
+++ preprocessing/utils/mda_util_dumbbells.py
@@ -0,0 +1,62 @@
+import os
+import sys
+
+import numpy as np
+import MDAnalysis as mda
+
+
+def create_mda(dcd_file, data_file): # loads trajectory with unwrapped coordinates
+    u = mda.Universe(data_file, dcd_file)
+    return u
+
+def check_files_exist(dcd_file, data_file):
+    print('Trying to find files at')
+    print(dcd_file)
+    print(data_file)
+    if os.path.isfile(dcd_file) and os.path.isfile(data_file):
+        print('Located dcd and data files.')
+    else:
+        print('Did not locate dcd and data files.')
+        sys.exit()
+    return
+
+def define_atom_types(u):
+    # sort atoms into type of molecule
+    anions_free = u.select_atoms("type Anion") # free anions
+    anions_paired = u.select_atoms("type Anion_Pair") # anions in dumbbell
+    anions = u.select_atoms("type Anion or type Anion_Pair") # all anions
+    cations_free = u.select_atoms("type Cation") # free cations
+    cations_paired = u.select_atoms("type Cation_Pair") # cations in dumbbell
+    cations = u.select_atoms("type Cation or type Cation_Pair") # all anions
+    solvent = u.select_atoms("type Solvent")
+    return cations, anions, solvent, anions_free, anions_paired, cations_free, cations_paired
+
+def create_position_arrays(u, anions_free, anions_paired, anions, cations, solvent):
+    # generate numpy arrays with all atom positions
+    # position arrays: [time, ion index, spatial dimension (x/y/z)]
+    time = 0
+    n_times = u.trajectory.n_frames
+    anion_paired_positions = np.zeros((n_times, len(anions_paired), 3))
+    anion_free_positions = np.zeros((n_times, len(anions_free), 3))
+    anion_positions = np.zeros((n_times, len(anions), 3))
+    cation_positions = np.zeros((n_times, len(cations), 3))
+    solvent_positions = np.zeros((n_times, len(solvent), 3))
+    for ts in u.trajectory:
+        anion_free_positions[time, :, :] = anions_free.positions - u.atoms.center_of_mass(pbc=True)
+        anion_paired_positions[time, :, :] = anions_paired.positions - u.atoms.center_of_mass(pbc=True)
+        anion_positions[time, :, :] = anions.positions - u.atoms.center_of_mass(pbc=True)
+        cation_positions[time, :, :] = cations.positions - u.atoms.center_of_mass(pbc=True)
+        solvent_positions[time, :, :] = solvent.positions - u.atoms.center_of_mass(pbc=True)
+        time += 1
+    return anion_free_positions, anion_paired_positions, anion_positions, cation_positions, solvent_positions
+
+def mda_to_numpy(conc, lb, ptd='../../data/md-trajectories/'):
+    dcd_file = '{}conc{}_lb{}.dcd'.format(ptd, conc, lb)
+    data_file = '{}initial_config_dumbbells_conc{}.gsd'.format(ptd, conc)
+    check_files_exist(dcd_file, data_file)
+    u = create_mda(dcd_file, data_file)
+    box_length = u.dimensions[0] # box length (use to wrap coordinates with periodic boundary conditions)
+    cations, anions, solvent, anions_free, anions_paired, _, _ = define_atom_types(u)
+    anion_free_positions, anion_paired_positions, anion_positions, cation_positions, solvent_positions = (create_position_arrays(u, anions_free, anions_paired, anions, cations, solvent))
+
+    return anion_free_positions, anion_paired_positions, anion_positions, cation_positions, solvent_positions, box_length
