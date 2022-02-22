fb902b3d46c17b8607882c8c844ef66ab6166e69
PenelopeJones <pj321@cam.ac.uk>
2022-02-22 10:27:08 +0000

    Analysis of full temporal trajectories

--- experiments/correlation-function-temporal.py
+++ experiments/correlation-function-temporal.py
@@ -9,10 +9,45 @@ import numpy as np
 
 import MDAnalysis as mda
 
-from utils.feature_util import static_feature_vector
-from utils.mda_util import check_files_exist, define_atom_types, create_position_arrays, create_mda
 
 import pdb
+
+def create_mda(dcd_file, data_file): # loads trajectory with unwrapped coordinates
+    u = mda.Universe(data_file, dcd_file)
+    return u
+
+def check_files_exist(dcd_file, data_file):
+    print('Trying to find files at')
+    print(dcd_file)
+    print(data_file)
+    if os.path.isfile(dcd_file) and os.path.isfile(data_file):
+        print('Located dcd and data files.')
+    else:
+        print('Did not locate dcd and data files.')
+        sys.exit()
+    return
+
+def define_atom_types(u):
+    # sort atoms into type of molecule
+    anions = u.select_atoms("type Anion")
+    cations = u.select_atoms("type Cation")
+    solvent = u.select_atoms("type Solvent")
+    return cations, anions, solvent
+
+def create_position_arrays(u, anions, cations, solvent):
+    # generate numpy arrays with all atom positions
+    # position arrays: [time, ion index, spatial dimension (x/y/z)]
+    time = 0
+    n_times = u.trajectory.n_frames
+    anion_positions = np.zeros((n_times, len(anions), 3))
+    cation_positions = np.zeros((n_times, len(cations), 3))
+    solvent_positions = np.zeros((n_times, len(solvent), 3))
+    for ts in u.trajectory:
+        anion_positions[time, :, :] = anions.positions - u.atoms.center_of_mass(pbc=True)
+        cation_positions[time, :, :] = cations.positions - u.atoms.center_of_mass(pbc=True)
+        solvent_positions[time, :, :] = solvent.positions - u.atoms.center_of_mass(pbc=True)
+        time += 1
+    return anion_positions, cation_positions, solvent_positions
 
 def mda_to_numpy(conc, lb, ptd='../../data/md-trajectories/'):
     dcd_file = '{}full_trajectories/conc{}_lb{}.dcd'.format(ptd, conc, lb)
